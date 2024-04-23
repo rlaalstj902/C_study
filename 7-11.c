1bd96cd594997fc03638175a4e0f3a1a3df74dc0
rlaalstj902 <162665482+rlaalstj902@users.noreply.github.com>
2024-04-23 19:14:26 +0900

    Add files via upload

--- 7-11.c
+++ 7-11.c
@@ -2,17 +2,17 @@
 
 void main()
 {
-	int hap = 0;
+	int hap;
 	int i;
 
-	for ( i =1; i <=100; i ++)
+	for ( i =1; i <=100; i++)
 	{
 		hap += i;
 
-		if (hap > 2000)
-			goto mygoto;
+			if (hap > 2000)
+				goto mygoto;
 	}
 
 mygoto:
-	printf("1부터 %d까지 합하면 2000이 넘어요. \n", i);
+	printf("1부터 %d까지 합하면 2000이 넘어요. \n",i);
 }
