42cf66ac6482dc747f0fb81c168b00f699fb5e35
rlaalstj902 <162665482+rlaalstj902@users.noreply.github.com>
2024-05-03 09:59:11 +0900

    Add files via upload

---
+++ 8-3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+	int aa[4];
+	int hap=0;
+	
+	for (int i=0; i <=3; i++)
+	{
+		printf("%d 번째 숫자를 입력하세요: ", i+1);
+		scanf("%d", &aa[i] );
+	}
+	
+	hap = aa[0] + aa[1] + aa[2] + aa[3];
+
+	printf(" 합계 ==> %d \n",hap);
+}
