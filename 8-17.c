0b3105dc9ecb8696dd25a1d63b9ce987089c3955
rlaalstj902 <162665482+rlaalstj902@users.noreply.github.com>
2024-05-10 10:31:48 +0900

    Add files via upload

---
+++ 8-17.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main()
+{
+	int gugu[9][9];
+	int i,k;
+	
+	for (i=0; i<9; i++) {
+		for(k=0; k<9; k++) {
+			gugu[i][k]=(k+2)*(i+1);
+		}
+	}
+	for (i=0; i<9; i++) {
+		for(k=0; k<9; k++){
+			printf("%dX%d=%2d\t", k+2,i+1,gugu[i][k]);
+		}
+		printf("\n");
+	}
+
+}
